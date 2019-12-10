3ef6330021ff61913e4dec5f56c730feeb842f56
EndyPratama <endypratama2999@gmail.com>
2019-12-10 08:04:51 +0700

    penambahan tampilan dan penambahan search berdasarkan nomer kamar

--- data rumah sakit.cpp
+++ data rumah sakit.cpp
@@ -4,6 +4,43 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <windows.h>
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD CursorPosition;
+
+void gotoXY(int x, int y){
+	CursorPosition.X = x;
+	CursorPosition.Y = y;
+	SetConsoleCursorPosition(console, CursorPosition);
+}
+
+void border(){
+	gotoXY(15,3);printf("**************************************************************\n");
+	gotoXY(15,4);printf("**************************************************************\n");
+	gotoXY(15,5);printf("**********                                          **********\n");
+	gotoXY(15,6);printf("**********                                          **********\n");
+	gotoXY(15,7);printf("**********                                          **********\n");
+	gotoXY(15,8);printf("****  ****                                          ****  ****\n");
+	gotoXY(15,9);printf("****  ****                                          ****  ****\n");
+	gotoXY(15,10);printf("****  ****                                          ****  ****\n");
+	gotoXY(15,11);printf("****  ****                                          ****  ****\n");
+	gotoXY(15,12);printf("**********                                          **********\n");
+	gotoXY(15,13);printf("**********                                          **********\n");
+	gotoXY(15,14);printf("**********                                          **********\n");
+	gotoXY(15,15);printf("**************************************************************\n");
+	gotoXY(15,16);printf("**************************************************************\n");
+}
+
+void list(int n){
+	int i,m;
+	m=n*9;
+	printf("**************************************************************\n");
+	for(i=0;i<m;i++){
+		printf("***                                                        ***\n");
+	}
+	printf("**************************************************************\n");
+}
 
 struct ttl{
 	int tanggal;
@@ -101,7 +138,7 @@ int strjump(char arr[][100], int jumlah, char data[][100]){
 		}
 	}
 	if (ketemu!=1){
-		printf("\nData Tidak Ditemukan !!!");
+		gotoXY(35,13);printf("Data Tidak Ditemukan !!!");
 		getch();
 	}
 	return index;
@@ -197,17 +234,18 @@ void strquick(char arr[][100], int awal, int akhir){
 int main(){
 	srand(time(0));
 	data dt[100];
-	int i,j,loop=1,menu,menu1,orang=0,n=0,dat=0,sort,ganti;
+	int a,b,i,j,loop=1,menu,menu1,orang=0,n=0,dat=0,sort,ganti;
 	char cganti[10],ccari[10];
 	while(loop==1){
-		printf("Draft Menu : \n");
-		printf("1. Input data\n");
-		printf("2. Tampilkan data\n");
-		printf("3. Cari data\n");
-		printf("4. Ganti data\n");
-		printf("5. Pengurutan data\n");
-		puts("0. X-it\n");
-		printf("Pilihan : ");
+		border();
+		gotoXY(35,6);printf("Draft Menu : \n");
+		gotoXY(35,7);printf("1. Input data\n");
+		gotoXY(35,8);printf("2. Tampilkan data\n");
+		gotoXY(35,9);printf("3. Cari data\n");
+		gotoXY(35,10);printf("4. Ganti data\n");
+		gotoXY(35,11);printf("5. Pengurutan data\n");
+		gotoXY(35,12);puts("0. X-it\n");
+		gotoXY(35,13);printf("Pilihan : ");
 		scanf("%d",&menu);
 		switch(menu){
 			case 1:{//input
@@ -242,17 +280,18 @@ int main(){
 				}
 				else{
 					system("cls");
+					list(n);
+					a=4,b=1;
 					for(j=0;j<n;j++){
-						printf("Data ke - %d",j+1);
-						printf("\n");
-						printf("ID Pasien\t: %d\n",dt[j].id);
-						printf("Nama Pasien\t: %s\n",dt[j].nama);
-						printf("Jenis Penyakit\t: %s\n",dt[j].penyakit);
-						printf("Jenis Kamar\t: %s\n",dt[j].kamar);
-						printf("Nomor Kamar\t: %d\n",dt[j].nomorkamar);
-						printf("Tanggal Masuk\t: %d\n",dt[j].tl.tanggal);
-						printf("Bulan Masuk\t: %d\n",dt[j].tl.bulan);
-						printf("\n\n");
+						gotoXY(a,b);printf("Data ke - %d",j+1);printf("\n");b++;
+						gotoXY(a,b);printf("ID Pasien\t\t: %d\n",dt[j].id);b++;
+						gotoXY(a,b);printf("Nama Pasien\t\t: %s\n",dt[j].nama);b++;
+						gotoXY(a,b);printf("Jenis Penyakit\t: %s\n",dt[j].penyakit);b++;
+						gotoXY(a,b);printf("Jenis Kamar\t\t: %s\n",dt[j].kamar);b++;
+						gotoXY(a,b);printf("Nomor Kamar\t\t: %d\n",dt[j].nomorkamar);b++;
+						gotoXY(a,b);printf("Tanggal Masuk\t: %d\n",dt[j].tl.tanggal);b++;
+						gotoXY(a,b);printf("Bulan Masuk\t\t: %d\n",dt[j].tl.bulan);b++;
+						b++;
 					}
 					getch();
 					system("cls");
@@ -260,61 +299,97 @@ int main(){
 				break;
 			}
 			case 3:{//search
-				int tempid[n], cari, index;
+				int tempid[n],tempnomorkamar[n], cari, index;
 				char coba[100][100];
 				char caristr[100][100];
-				printf("List : \n");
-				printf("1. ID\n");
-				printf("2. Nama\n");
-				printf("3. Nomor Kamar\n");
-				printf("Pilih : ");scanf("%d",&menu1);fflush(stdin);
+				system("cls");
+				border();
+				gotoXY(38,7);printf("List : \n");
+				gotoXY(38,8);printf("1. ID\n");
+				gotoXY(38,9);printf("2. Nama\n");
+				gotoXY(38,10);printf("3. Nomor Kamar\n");
+				gotoXY(38,11);printf("Pilih : ");scanf("%d",&menu1);fflush(stdin);
 				switch (menu1){
-					case 1:
-						for (int i=0; i<n; i++){
+					case 1://id
+						for (int i=0; i<=n; i++){
 							tempid[i] = dt[i].id;
 						}
-						printf("\nMasukkan ID Pasien Yang Ingin Dicari = ");scanf("%d",& cari);fflush(stdin);
+						gotoXY(26,12);printf("Masukkan ID Pasien Yang Ingin Dicari = ");scanf("%d",& cari);fflush(stdin);
 						index = jump(tempid, n, cari);	
 						system("cls");
-						if (index!=-1){
-							printf("Data ke - %d",index+1);
-							printf("\n");
-							printf("ID Pasien\t: %d\n",dt[index].id);
-							printf("Nama Pasien\t: %s\n",dt[index].nama);
-							printf("Jenis Penyakit\t: %s\n",dt[index].penyakit);
-							printf("Jenis Kamar\t: %s\n",dt[index].kamar);
-							printf("Nomor Kamar\t: %d\n",dt[index].nomorkamar);
-							printf("Tanggal Masuk\t: %d\n",dt[index].tl.tanggal);
-							printf("Bulan Masuk\t: %d\n",dt[index].tl.bulan);
-							getch();
-						}
+//						if (index!=-1){
+//							list(n);
+//							a=4;b=1;
+//							gotoXY(a,b);printf("Data ke - %d",index+1);printf("\n");b++;
+//							gotoXY(a,b);printf("ID Pasien\t: %d\n",dt[index].id);b++;
+//							gotoXY(a,b);printf("Nama Pasien\t: %s\n",dt[index].nama);b++;
+//							gotoXY(a,b);printf("Jenis Penyakit\t: %s\n",dt[index].penyakit);b++;
+//							gotoXY(a,b);printf("Jenis Kamar\t: %s\n",dt[index].kamar);b++;
+//							gotoXY(a,b);printf("Nomor Kamar\t: %d\n",dt[index].nomorkamar);b++;
+//							gotoXY(a,b);printf("Tanggal Masuk\t: %d\n",dt[index].tl.tanggal);b++;
+//							gotoXY(a,b);printf("Bulan Masuk\t: %d\n",dt[index].tl.bulan);b++;
+//							getch();
+//						}
 						system("cls");
 						break;
-					case 2:
+					case 2://nama
 						for (int i=0; i<=n; i++){
 							strcpy(coba[i], dt[i].nama);
 						}
 						fflush(stdin);
-						printf("\nMasukkan Nama Pasien Yang Ingin Dicari = ");gets(caristr[0]);
+						gotoXY(26,12);printf("Masukkan Nama Pasien Yang Ingin Dicari = ");gets(caristr[0]);
 						index = strjump(coba, n, caristr);
 						system("cls");
-						if (index!=-1){
-							printf("Data ke - %d",index+1);
-							printf("\n");
-							printf("ID Pasien\t: %d\n",dt[index].id);
-							printf("Nama Pasien\t: %s\n",dt[index].nama);
-							printf("Jenis Penyakit\t: %s\n",dt[index].penyakit);
-							printf("Jenis Kamar\t: %s\n",dt[index].kamar);
-							printf("Nomor Kamar\t: %d\n",dt[index].nomorkamar);
-							printf("Tanggal Masuk\t: %d\n",dt[index].tl.tanggal);
-							printf("Bulan Masuk\t: %d\n",dt[index].tl.bulan);
-							getch();
-						}
+//						if (index!=-1){
+//							printf("Data ke - %d",index+1);
+//							printf("\n");
+//							printf("ID Pasien\t: %d\n",dt[index].id);
+//							printf("Nama Pasien\t: %s\n",dt[index].nama);
+//							printf("Jenis Penyakit\t: %s\n",dt[index].penyakit);
+//							printf("Jenis Kamar\t: %s\n",dt[index].kamar);
+//							printf("Nomor Kamar\t: %d\n",dt[index].nomorkamar);
+//							printf("Tanggal Masuk\t: %d\n",dt[index].tl.tanggal);
+//							printf("Bulan Masuk\t: %d\n",dt[index].tl.bulan);
+//							getch();
+//						}
 						system("cls");
 						break;
-					case 3:
+					case 3://nomer kamar
+						for (int i=0; i<n; i++){
+							tempnomorkamar[i] = dt[i].nomorkamar;
+						}
+						gotoXY(26,12);printf("Masukkan No Kamar Yang Ingin Dicari = ");scanf("%d",& cari);fflush(stdin);
+						index = jump(tempnomorkamar, n, cari);	
+						system("cls");
+//						if (index!=-1){
+//							printf("Data ke - %d",index+1);
+//							printf("\n");
+//							printf("ID Pasien\t: %d\n",dt[index].id);
+//							printf("Nama Pasien\t: %s\n",dt[index].nama);
+//							printf("Jenis Penyakit\t: %s\n",dt[index].penyakit);
+//							printf("Jenis Kamar\t: %s\n",dt[index].kamar);
+//							printf("Nomor Kamar\t: %d\n",dt[index].nomorkamar);
+//							printf("Tanggal Masuk\t: %d\n",dt[index].tl.tanggal);
+//							printf("Bulan Masuk\t: %d\n",dt[index].tl.bulan);
+//							getch();
+//						}
+						system("cls");
 						break;
 				}
+				if (index!=-1){
+					list(1);
+					a=4;b=1;
+					gotoXY(a,b);printf("Data ke - %d",index+1);printf("\n");b++;
+					gotoXY(a,b);printf("ID Pasien\t\t: %d\n",dt[index].id);b++;
+					gotoXY(a,b);printf("Nama Pasien\t\t: %s\n",dt[index].nama);b++;
+					gotoXY(a,b);printf("Jenis Penyakit\t: %s\n",dt[index].penyakit);b++;
+					gotoXY(a,b);printf("Jenis Kamar\t\t: %s\n",dt[index].kamar);b++;
+					gotoXY(a,b);printf("Nomor Kamar\t\t: %d\n",dt[index].nomorkamar);b++;
+					gotoXY(a,b);printf("Tanggal Masuk\t: %d\n",dt[index].tl.tanggal);b++;
+					gotoXY(a,b);printf("Bulan Masuk\t\t: %d\n",dt[index].tl.bulan);b++;
+					getch();
+				}
+				system("cls");
 				break;
 			}
 			case 4:{//ubah
@@ -324,24 +399,24 @@ int main(){
 				}
 				system("cls");
 				fflush(stdin);
-				printf("\nMasukkan NomorID yang ingin anda ubah : ");
-				scanf("%d",& ganti);
-				fflush(stdin);
+				list(1);
+				a=4;b=1;
+				gotoXY(a,b);printf("Masukkan NomorID yang ingin anda ubah : ");scanf("%d",& ganti);fflush(stdin);b++;
 				index = jump(tempid, n, ganti);
 				if(index!=-1){
-					printf("Masukkan data baru\n");
-					printf("Nama Pasien\t\t: ");
+					gotoXY(a,b);printf("Masukkan data baru\n");b++;
+					gotoXY(a,b);printf("Nama Pasien\t\t: ");b++;
 					gets(dt[index].nama);
-					printf("Jenis Penyakit\t\t: ");
+					gotoXY(a,b);printf("Jenis Penyakit\t: ");b++;
 					gets(dt[index].penyakit);
-					printf("Jenis Kamar\t\t: ");
+					gotoXY(a,b);printf("Jenis Kamar\t\t: ");b++;
 					gets(dt[index].kamar);
-					printf("Nomor Kamar\t\t: ");
+					gotoXY(a,b);printf("Nomor Kamar\t\t: ");b++;
 					scanf("%d",& dt[index].nomorkamar);
 					fflush(stdin);
-					printf("Tanggal masuk\t\t: ");
+					gotoXY(a,b);printf("Tanggal masuk\t: ");b++;
 					scanf("%d",&dt[index].tl.tanggal);
-					printf("Bulan masuk\t\t: ");
+					gotoXY(a,b);printf("Bulan masuk\t\t: ");b++;
 					scanf("%d",&dt[index].tl.bulan);
 					system("cls");
 					printf("Data Berhasil Diupdate !!!");
@@ -351,6 +426,7 @@ int main(){
 				break;
 			}
 			case 5:{//sorting
+				system("cls");
 				char temp[100][100];
 				int tempid[n];
 				char tempnama[10];
@@ -359,15 +435,16 @@ int main(){
 				int tempnomorkamar[n];
 				int temptanggal[n];
 				int tempbulan[n];
-				printf("List : \n");
-				printf("1. ID\n");
-				printf("2. Nama\n");
-				printf("3. Penyakit\n");
-				printf("4. Kamar\n");
-				printf("5. No. Kamar\n");
-				printf("6. Tanggal\n");
-				printf("7. Bulan\n");
-				printf("Pilih : ");scanf("%d",&menu1);fflush(stdin);
+				border();
+				gotoXY(35,5);printf("List : \n");
+				gotoXY(35,6);printf("1. ID\n");
+				gotoXY(35,7);printf("2. Nama\n");
+				gotoXY(35,8);printf("3. Penyakit\n");
+				gotoXY(35,9);printf("4. Kamar\n");
+				gotoXY(35,10);printf("5. No. Kamar\n");
+				gotoXY(35,11);printf("6. Tanggal\n");
+				gotoXY(35,12);printf("7. Bulan\n");
+				gotoXY(35,13);printf("Pilih : ");scanf("%d",&menu1);fflush(stdin);
 				switch(menu1){
 					case 1:
 						for (int i=0; i<=n; i++){
@@ -666,6 +743,7 @@ int main(){
 			}
 			case 0:
 				loop=0;
+				gotoXY(0,20);exit(0);
 				break;
 		}
 	}	
